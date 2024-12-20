a92eeaa2c5a5094da5115aa68c06edcf686128db
irfan_usuf <irfanusuf33@gmail.com>
2024-12-20 12:52:29 +0530

    fixes

--- Project 4 :  server side js /controllers/userController.js
+++ Project 4 :  server side js /controllers/userController.js
@@ -1,5 +1,7 @@
 const { User } = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
+const { messageHandler } = require("../utils/messagehandler");
 
 const createUser = async (req, res) => {
   try {
@@ -35,4 +37,43 @@ const createUser = async (req, res) => {
   }
 };
 
-module.exports = { createUser };
+const loginhandler = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return messageHandler(res , 400 , false , "Both feilds Required!");
+    }
+
+    const findUser = await User.findOne({ email }); // db query  // return user object
+
+    if (!findUser) {
+      return messageHandler(res, 404, false , "User not Found");
+    }
+
+    const comparePass = await bcrypt.compare(password, findUser.password);
+
+    if (comparePass) {
+
+      const userId = findUser._id
+      const secretKey = "randomString"
+
+      const token = await jwt.sign({userId} , secretKey )
+
+      res.cookie('token', token, { httpOnly: true, maxAge: 3600000 });    // i m sending cookie directly no need of sending payload
+
+      return  messageHandler(res, 200, true , "Logged In succesfully!" , token);     // u can just remove the token from the payload
+    } else {
+     return messageHandler(res, 400, false ,  "Incorrect PassWord");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+
+
+
+
+module.exports = { createUser, loginhandler };
--- Project 4 :  server side js /index.js
+++ Project 4 :  server side js /index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const cors = require("cors")
 const { fetchData } = require("./controllers/dataController")
 const { connectDb } = require("./config/connectDb")
-const { createUser } = require("./controllers/userController")
+const { createUser, loginhandler } = require("./controllers/userController")
 
 
 const server = express()
@@ -22,7 +22,7 @@ server.get("/" , (req,res)=>{res.send("Hello from the server. this server is mad
 
 server.post("/fetch-data" , fetchData )
 server.post("/user/register" , createUser)
-
+server.post("/user/login" , loginhandler)
 
 
 
