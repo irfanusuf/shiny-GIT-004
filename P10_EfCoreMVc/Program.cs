95d20f9272cefcb172b446787069816364e68ad4
irfan_usuf <irfanusuf33@gmail.com>
2025-02-06 17:25:34 +0530

    fixes

---
+++ P10_EfCoreMVc/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Interfaces;
+using WebApplication1.Services;
+using WebApplication1.Structs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<SqlDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("main")));
+
+
+
+builder.Services.AddSingleton<ITokenService, TokenService>();
+builder.Services.AddSingleton<IMailService , EmailService>();
+builder.Services.AddScoped<ICloudinaryService, CloudinaryService>();
+
+
+
+LoadEnvStruct loadEnv  = new();    //
+
+var app = builder.Build();
+
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");  
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
--- Project 10: Ef core mvc/WebApplication1/Interfaces/ICloudinary.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace WebApplication1.Interfaces;
-
-public interface ICloudinary
-{
-public void CreatePath(string userId , string username , string email);
-}
