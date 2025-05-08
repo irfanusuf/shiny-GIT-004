df856f5cbac6db5b70b4d51341d6f8b856710709
irfan_usuf <irfanusuf33@gmail.com>
2025-05-08 12:43:01 +0530

    fixes

--- P3_fuzzyGoggle/redux2/src/utils/apiCall.js
+++ P3_fuzzyGoggle/redux2/src/utils/apiCall.js
@@ -1,46 +1,54 @@
 import { axiosInstance } from "./axiosInstance";
 
-export const apiCall = (url, e , formData) => async (action) => {
-  try {
-    if (e !== undefined) {
-      e.preventDefault();
-    }
+export const apiCall =
+  (requsetType, url, e, formData, Id) => async (action) => {
+    try {
+      if (e !== undefined) {
+        e.preventDefault();
+      }
 
-    let res;
+      let res;
 
-    action({ type: "API_REQUEST" }); // state.loading :true
+      action({ type: "API_REQUEST" }); // state.loading :true
 
-    console.log(formData);
+      if (formData !== undefined && e !== undefined) {
+        if (requsetType === "post") {
+          res = await axiosInstance.post(`${url}`, formData);
+        } else if (requsetType === "put" && Id !== undefined) {
+          res = await axiosInstance.put(`${url}/${Id}`, formData);
+        }
+      } else {
+        if (requsetType === "get") {
+          if (Id !== undefined) {
+            res = await axiosInstance.get(`url/${Id}`);
+          } else {
+            res = await axiosInstance.get(url);
+          }
+        }
+      }
 
-    if (formData !== undefined) {
-      res = await axiosInstance.post(`${url}`, formData);
-    } else {
-      res = await axiosInstance.get(url);
-    }
+      if (res.status === 200) {
+        action({
+          type: "API_SUCCESS",
+          payload: res.data.payload,
+          message: res.data.message,
+          username: res.data.username,
+          email: res.data.email,
+        });
 
+        return true;
+      } else {
+        return false;
+      }
+    } catch (err) {
+      console.log(err);
 
-
-    if (res.status === 200) {
       action({
-        type: "API_SUCCESS",
-        payload: res.data.payload,
-        message: res.data.message,
-        username: res.data.username,
-        email: res.data.email,
+        type: "API_FAILURE",
+        error: err,
+        message: err.response.data.message,
       });
 
-      return true;
-    } else {
       return false;
     }
-  } catch (err) {
-    console.log(err);
-    action({
-      type: "API_FAILURE",
-      error: err,
-      message: err.message,
-    });
-
-    return false;
-  }
-};
+  };
